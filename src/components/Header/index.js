b742f4a9de5e8a2339f14cfb43e7a49e130838d2
Jeff Maxwell <maxjeffwell@gmail.com>
2019-02-19 21:18:27 -0500

    add header and footer components

---
+++ src/components/Header/index.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Menu } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const StyledMenu = styled(Menu)`
+  &&& {
+    display: flex;
+    border: 2px solid ${props => props.theme.orange};
+    border-radius: 5px;
+    margin-bottom: 25px;
+    flex-wrap: wrap;
+    min-width: 320px;
+    height: auto;
+  }
+  &&& .header {
+    background: ${props => props.theme.green};
+  }
+  &&& a.header.item {
+    width: 50px;
+    position: absolute;
+  }
+  &&& a.item {
+   font-size: 2em;
+   font-weight: bold;
+   font-family: 'Roboto', 'sans-serif';
+   color: ${props => props.theme.green};
+   text-align: center;
+  }
+  &&& header.item {
+    background-color: ${props => props.theme.green};
+    width: 75px;
+  }
+`;
+
+class Header extends Component {
+  showLinks() {
+    if (this.props.authenticated) {
+      return (
+        <StyledMenu>
+          <Menu.Item as="header">
+          </Menu.Item>
+          <Menu.Menu position="left">
+            <Menu.Item as={Link} name="Instructor Dashboard" to="/dashboard">
+            </Menu.Item>
+          </Menu.Menu>
+          <Menu.Menu position="right">
+            <Menu.Item as={Link} name="Students" to="/students">
+            </Menu.Item>
+            <Menu.Item as={Link} name="Logout" to="/signout">
+            </Menu.Item>
+          </Menu.Menu>
+        </StyledMenu>
+      );
+    } else {
+      return (
+        <StyledMenu>
+          <Menu.Menu position="left">
+            <Menu.Item as="header">
+            </Menu.Item>
+          </Menu.Menu>
+          <Menu.Menu>
+            <Menu.Item as={Link} name="educationelly" to="/">
+              educationELLy
+            </Menu.Item>
+          </Menu.Menu>
+          <Menu.Menu position="right">
+            <Menu.Item as={Link} name="Register" to="/Signup">
+            </Menu.Item>
+            <Menu.Item as={Link} name="Login" to="/Signin">
+            </Menu.Item>
+          </Menu.Menu>
+        </StyledMenu>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <Menu.Header>
+        {this.showLinks()}
+      </Menu.Header>
+    );
+  };
+}
+
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -2,37 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
-import { getMainDefinition } from 'apollo-utilities';
-import { ApolloLink, split } from 'apollo-link';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
-import { WebSocketLink } from 'apollo-link-ws';
 import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import WebFont from 'webfontloader';
+
+import 'semantic-ui-css/components/button.css';
+import 'semantic-ui-css/components/container.css';
+import 'semantic-ui-css/components/grid.css';
+import 'semantic-ui-css/components/header.css';
+import 'semantic-ui-css/components/form.css';
+import 'semantic-ui-css/components/icon.css';
+import 'semantic-ui-css/components/segment.css';
+import 'semantic-ui-css/components/message.css';
+import 'semantic-ui-css/components/card.css';
+import 'semantic-ui-css/components/menu.css';
+import 'semantic-ui-css/components/input.css';
+import 'semantic-ui-css/components/sidebar.css';
 
 import App from './components/App';
 import { signOut } from './components/SignOut';
 
+WebFont.load({
+  google: {
+    families: ['Roboto: 400', 'sans-serif']
+  },
+  timeout: 2000
+});
+
 const httpLink = new HttpLink({
-  uri: process.env.NODE_ENV === 'development' ? SERVER_URL : SERVER_URL_PROD
+  uri: 'http://localhost:8000/graphql'
 });
-
-const wsLink = new WebSocketLink({
-  uri: process.env.NODE_ENV === 'development' ? WEBSOCKET_ENDPOINT : WEBSOCKET_ENDPOINT_PROD,
-  options: {
-    reconnect: true,
-  },
-});
-
-const terminatingLink = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return (
-      kind === 'OperationDefinition' && operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink,
-);
 
 const authLink = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => {
@@ -68,7 +70,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   }
 });
 
-const link = ApolloLink.from([authLink, errorLink, terminatingLink]);
+const link = ApolloLink.from([authLink, errorLink, httpLink]);
 
 const cache = new InMemoryCache();
 
@@ -78,9 +80,45 @@ const client = new ApolloClient({
   connectToDevTools: true
 });
 
+const theme = {
+  orange: '#fb9438',
+  blue: '#2873b4',
+  green: '#86c64e',
+  white: '#f5f5f5',
+};
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: url('https://fonts.googleapis.com/css?family=Roboto');
+  }
+
+html {
+  box-sizing: border-box;
+  font-size: 14px;
+  }
+
+*, *:before, *:after {
+		box-sizing: inherit; // then inherit box sizing on everything else
+	}
+	
+body {
+	padding: 0;
+	margin: 0;
+	font-size: 1.5rem;
+	line-height: 2;
+	font-family: Roboto, sans-serif;
+	}
+`;
+
 ReactDOM.render(
+  <ThemeProvider theme={theme}>
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>,
+    <GlobalStyle />
+  </ApolloProvider>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
