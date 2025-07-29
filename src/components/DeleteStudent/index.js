840c0523f07837457ae5e453a4c483e18dd57472
Jeff M <maxjeffwell@gmail.com>
2025-07-29 05:49:20 -0400

    Change delete mutation parameter from _id to id

--- src/components/DeleteStudent/index.js
+++ src/components/DeleteStudent/index.js
@@ -41,8 +41,8 @@ const StyledDeleteButton = styled.button`
 `;
 
 const DELETE_STUDENT_MUTATION = gql`
-  mutation deleteStudent($_id: ID!) {
-    deleteStudent(_id: $_id) {
+  mutation deleteStudent($id: ID!) {
+    deleteStudent(id: $id) {
       _id
     }
   }
@@ -63,7 +63,7 @@ const StudentDelete = ({ studentId }) => {
     if (window.confirm('Are you sure you want to delete this student?')) {
       try {
         await deleteStudent({
-          variables: { _id: studentId },
+          variables: { id: studentId },
         });
         console.log('Student deleted successfully');
       } catch (error) {
